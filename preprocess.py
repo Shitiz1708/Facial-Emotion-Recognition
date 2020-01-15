d0ce3ae5451fe45f7c2ba2e2e36365583bf398e0
Shitiz Gupta <guptashitiz17@gmail.com>
2020-01-16 00:50:33 +0530

    Initial Changes

--- preprocess.py
+++ preprocess.py
@@ -1,34 +1,53 @@
-from keras.preprocessing.image import load_img,img_to_array
+# from keras.preprocessing.image import load_img,img_to_array
 import os
 import math
 import numpy as np
+import cv2
 
 X_train=[]
 y_train = []
 
-emotion_folder = os.listdir('/Emotion')
+emotion_folder = os.listdir('./Emotion')
 for i in emotion_folder:
-    inner_folder = os.listdir('/Emotion/'+i+'/')
+    # print(i)
+    inner_folder = os.listdir('./Emotion/'+i+'/')
+    print(inner_folder)
     for j in inner_folder:
-        txtfile = os.listdir('/Emotion/' + i +'/' + j + '/')
+        # print(j)/
+        print('./Emotion/' + str(i) +'/' + str(j) + '/')
+        txtfile = os.listdir('./Emotion/' + i +'/' + j + '/')
+        print(txtfile)
         if len(txtfile)!=0:
-            with open(txtfile[0],'r') as file:
+            with open('./Emotion/' + i +'/' + j + '/'+txtfile[0],'r') as file:
                 label = int(file.read().split(' ')[3][0])
+                print(label)
                 y_train.append(label)
-            path = '/cohn-kanade-images/' + i + '/' + j + '/'
+            path = './cohn-kanade-images/' + i + '/' + j + '/'
             images1 = sorted(os.listdir(path),key=len)
+            if '.DS_Store' in images1:
+                images1.remove('.DS_Store')
+            
             images = []
-            if len(images1)>7:
-                # images = sorted(np.random.choice(images,7))
-                for i in range(0,len(images1),len(images1)/7):
-                    images.append(images1[i])
+            # print(len(images1))
+            if len(images1)>6:
+                images = sorted(np.random.choice(images1,6))
+                # for k in range(0,len(images1),int(len(images1)/7)):
+                #     images.append(images1[k])
             else:
                 images = images1
+            print(len(images))
+            print(images)
             imgs = []
             for image in images:
-                im = load_img(path + image)
-                im = img_to_array(im,data_format="channels_first")
+                # print(image)
+                im = cv2.imread(path+ image)
+                im = cv2.resize(im,(640,480),interpolation = cv2.INTER_AREA)
+                # im = load_img(path + image)
+                # im = img_to_array(im,data_format="channels_first")
                 imgs.append(im)
+                print(im.shape)
+            imgs = np.array(imgs)
+            print(imgs.shape)
             X_train.append(imgs)
 
 X_train = np.array(X_train)
